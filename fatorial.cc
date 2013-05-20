24b37c33f55b811bccc5891c0eeb39a6dd6a9e03
Renan <renanteruoc@gmail.com>
2013-05-20 04:27:58 -0300

    lkj

--- fatorial.cc
+++ fatorial.cc
@@ -10,27 +10,14 @@ using namespace std;
 
 mpf_class precalc[100];
 
-/*int fatinit(int n){
-  int i;
-  precalc = malloc(n*sizeof(*precalc));
-  if (!precalc)
-    return 0;
-  for(i = 0; i < n; i++)
-    precalc[i] = 0;
-  return 1;
-  }*/
-
 
 mpf_class fatorial(int n){
-  //printf("int -> %d\n", n);
   if (!cmp(precalc[n],0)){
-    //cout << "POOP\n";
     if (n==0)
      precalc[n] = 1;
     else{
       precalc[n] =  fatorial(n-1) * n;
     }
   }
-  cout << precalc[n] << " precalc\n";
   return precalc[n];
 }
--- main.cc
+++ main.cc
@@ -17,33 +17,18 @@ mpf_class iteration(int k){
   mpf_class upsummagicnumber = 13591409;
   mpf_class upmultmagicnumber = 545140134;
   mpf_class downmagicnumber = -640320;
-  cout << upsummagicnumber << "\n";
-  cout << upmultmagicnumber << "\n";
-  cout << downmagicnumber << "\n";
   mpf_class upfat = (fatorial( 6 * k ));
-  cout << upfat << " upfat\n";
-  cout << "iprest\n";
   mpf_class upresto = upmultmagicnumber*k;
-  cout << upresto << "\n";
   upresto = upresto + upsummagicnumber;
-  cout << "pizza\n";
-  cout << upresto << " uprestod=fin\n";
   mpf_class downfat = fatorial(3*k);
-  cout << downfat << " downdat\n";
   mpf_pow_ui(tmp, fatorial(k).get_mpf_t(), 3);
   mpf_class downfatexp = mpf_class(tmp);
-  cout << "a\n";
   mpf_pow_ui(tmp, downmagicnumber.get_mpf_t(), 3*k);
   mpf_class downresto = mpf_class(tmp); 
-  cout << downresto << " ihnis\n";
   mpf_class up = upfat * upresto;
   mpf_class down = (downfat * downfatexp);
   down = down* downresto;
-  cout << up << "\n";
-  cout << down << "\n";
-  cout << "moo\n";
   mpf_class returning = up/down;
-  cout << "cow\n";
   return returning;
 }
 
@@ -58,7 +43,6 @@ int main(int argc, char **argv){
   //  fatinit(100);
   //#pragma omp parallel private()
   for(i = 0; i < 1; i++){
-    cout << "pi\n";
     mynotpi = mynotpi + iteration(i);
   }
   mynotpi =  leftside/mynotpi;
